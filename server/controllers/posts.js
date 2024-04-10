1fdb9e5f06f0e8f96ccf0d65c6a5dee8362fba7c
Harshal Rajput <harshalrajput9000@gmail.com>
2024-04-10 16:59:00 +0530

    Remove Space from newPost to new Post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,7 +8,7 @@ export const createPost = async (req, res) => {
     const { userId, description, picturePath } = req.body;
     const user = await User.findById(userId);
 
-    const newPost = newPost({
+    const newPost = new Post({
       userId,
       firstName: user.firstName,
       lastName: user.lastName,
@@ -33,7 +33,7 @@ export const createPost = async (req, res) => {
 export const getFeedPosts = async (req, res) => {
   try {
     const post = await Post.find();
-    res.status(201).json(post);
+    res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
